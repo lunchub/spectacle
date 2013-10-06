0f6ad8464c060b463997f072a44a45b1e3c1539c
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-06 17:45:43 +0900

    Fix method to FlowchartObeserver

--- app/models/flowchart_observer.rb
+++ app/models/flowchart_observer.rb
@@ -1,14 +1,17 @@
 class FlowchartObserver < ActiveRecord::Observer
 
-  def after_save(flowchart)
+  def before_save(flowchart)
     dir_path = "#{Rails.root}/public/flowcharts"
     FileUtils.mkdir_p(dir_path) unless File.exist?(dir_path)
 
     Graph do
       eval(flowchart.dot)
 
-      save("#{dir_path}/#{flowchart.scenario.title}", :png)
+      save("#{dir_path}/#{flowchart.scenario.title}", :svg)
     end
+
+    flowchart.image = "/flowcharts/#{flowchart.scenario.title}.svg"
   end
 
 end
+
