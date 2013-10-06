c78fe5387fb81bbc46a82613d653f8ec29c5538c
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-06 17:45:23 +0900

    Added validation to Scenario

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,4 +1,8 @@
 class Scenario < ActiveRecord::Base
   has_one :flowchart, dependent: :destroy
   accepts_nested_attributes_for :flowchart, :allow_destroy => true
+
+  validates_presence_of :title
+  validates_uniqueness_of :title
+
 end
