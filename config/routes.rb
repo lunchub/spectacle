d72378a145a0c82db2c6342b356cf16c65db2c31
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-06 16:33:47 +0900

    Fix root page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Spectacle::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'flowcharts#index'
+  root 'scenarios#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
