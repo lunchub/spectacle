5223f07c12f6a88042e72b4efc00f6be2079b70e
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-06 17:06:57 +0900

    Move method from Flowchart to FlowchartObserver

--- app/models/flowchart.rb
+++ app/models/flowchart.rb
@@ -1,12 +1,3 @@
 class Flowchart < ActiveRecord::Base
   belongs_to :scenario
-
-  def build
-    flowchart = self
-    Graph do
-      eval(flowchart.dot)
-
-      save("#{Rails.root}" + flowchart.scenario.name, :png)
-    end
-  end
 end
--- app/models/flowchart_observer.rb
+++ app/models/flowchart_observer.rb
@@ -1,2 +1,11 @@
 class FlowchartObserver < ActiveRecord::Observer
+
+  def after_save(flowchart)
+    Graph do
+      eval(flowchart.dot)
+
+      save("#{Rails.root}/public/flowcharts/#{flowchart.scenario.title}", :png)
+    end
+  end
+
 end
