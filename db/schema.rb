19ba8a04a5aa8f852251e054963ddce061fd925a
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-07 10:54:47 +0900

    Added Page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007015259) do
+ActiveRecord::Schema.define(version: 20131007015419) do
 
   create_table "flowcharts", force: true do |t|
     t.datetime "created_at"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20131007015259) do
     t.text     "dot"
     t.integer  "scenario_id"
     t.string   "image"
+  end
+
+  create_table "pages", force: true do |t|
+    t.string   "title"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "questions", force: true do |t|
