5058adb5baf4153c094eb5a063395610176631ab
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-23 22:24:19 +0900

    Refactor Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,16 +1,9 @@
 class Page < ActiveRecord::Base
   # has_and_belongs_to_many :scenario
 
+  # 1:N
   has_many :page_goals, dependent: :destroy
   accepts_nested_attributes_for :page_goals, reject_if: :all_blank, allow_destroy: true
-
-  has_many :page_site_options
-  has_many :site_options, through: :page_site_options
-  accepts_nested_attributes_for :page_site_options, reject_if: :all_blank, allow_destroy: true
-
-  has_many :page_mail_deliveries
-  has_many :mail_deliveries, through: :page_mail_deliveries
-  accepts_nested_attributes_for :page_mail_deliveries, reject_if: :all_blank, allow_destroy: true
 
   has_many :page_preconditions, dependent: :destroy
   accepts_nested_attributes_for :page_preconditions, reject_if: :all_blank, allow_destroy: true
@@ -21,14 +14,20 @@ class Page < ActiveRecord::Base
   has_many :page_actions, dependent: :destroy
   accepts_nested_attributes_for :page_actions, reject_if: :all_blank, allow_destroy: true
 
-  has_many :page_links, dependent: :destroy
-  accepts_nested_attributes_for :page_links, allow_destroy: true
-
   has_many :questions, as: :questionable, dependent: :destroy
   accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 
   has_many :page_images, dependent: :destroy
   accepts_nested_attributes_for :page_images, reject_if: :all_blank, allow_destroy: true
+
+  # N:M (through)
+  has_many :page_site_options
+  has_many :site_options, through: :page_site_options
+  accepts_nested_attributes_for :page_site_options, reject_if: :all_blank, allow_destroy: true
+
+  has_many :page_mail_deliveries
+  has_many :mail_deliveries, through: :page_mail_deliveries
+  accepts_nested_attributes_for :page_mail_deliveries, reject_if: :all_blank, allow_destroy: true
 
 
   validates :title, presence: true
