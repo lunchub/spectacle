4aef9af71227540d1a787167f49d02b29a7a71e1
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-23 16:28:47 +0900

    Update Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -16,4 +16,40 @@ class Page < ActiveRecord::Base
   has_many :questions, as: :questionable, dependent: :destroy
 
   has_many :page_images, dependent: :destroy
+
+  def has_page_goals?
+    self.page_goals.present?
+  end
+
+  def has_site_options?
+    self.site_options.present?
+  end
+
+  def has_mail_deliveries?
+    self.mail_deliveries.present?
+  end
+
+  def has_page_preconditions?
+    self.page_preconditions.present?
+  end
+
+  def has_page_specs?
+    self.page_specs.present?
+  end
+
+  def has_page_actions?
+    self.page_actions.present?
+  end
+
+  def has_page_links?
+    self.page_links.present?
+  end
+
+  def has_questions?
+    self.questions.present?
+  end
+
+  def has_page_images?
+    self.page_images.present?
+  end
 end
