7466e280002a74189ab0e39d297eae2b127145c4
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-23 13:02:23 +0900

    Added associations to Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,17 @@
 class Page < ActiveRecord::Base
-  has_and_belongs_to_many :scenario
+  # has_and_belongs_to_many :scenario
+
+  has_many :page_goals, dependent: :destroy
+
+  has_many :site_options, dependent: :destroy
+  has_many :mail_deliveries, dependent: :destroy
+
+  has_many :page_preconditions, dependent: :destroy
+  has_many :page_specs, dependent: :destroy
 
   has_many :page_actions, dependent: :destroy
+
+  has_many :page_links, dependent: :destroy
+
+  has_many :questions, as: :questionable, dependent: :destroy
 end
