a0d197b70a1e8faaf6176c23e57041541febc3ba
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-24 13:40:42 +0900

    Fix association between Question and Answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   belongs_to :questionable, polymorphic: true
 
   has_many :answers, dependent: :destroy
+  accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 end
