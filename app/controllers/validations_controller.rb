debab4606e84c40f9f604c67f9e932cfe59d050a
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-10-28 11:47:04 +0900

    Added StrongParameter into ValidationsController

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -69,6 +69,6 @@ class ValidationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def validation_params
-      params.require(:validation).permit(:name)
+      params.require(:validation).permit(:name, :detail)
     end
 end
